4fa6acf826d376576c34c34c875bb8fc78f67d5e
Sumit Kumar Mishra <mishra.sumitkumar@hotmail.com>
2023-08-16 17:56:56 +0530

    File Added

---
+++ MorningAssignment/StrongNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace MorningAssignment;
+
+internal class StrongNumber
+{
+    public static int Factorial(int num)
+    {
+        if (num == 0 || num == 1)
+            return 1;
+
+        return num * Factorial(num - 1);
+    }
+
+    public bool IsStrongNumber(int num)
+    {
+        int originalNum = num;
+        int sum = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += Factorial(digit);
+            num /= 10;
+        }
+        return sum == originalNum;
+    }
+}
