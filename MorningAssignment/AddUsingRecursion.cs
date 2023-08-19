089f6e06ea835a0bdaddc8bb1edb20848850a735
Sumit Kumar Mishra <mishra.sumitkumar@hotmail.com>
2023-08-19 18:38:10 +0530

    #NEW

---
+++ MorningAssignment/AddUsingRecursion.cs
@@ -0,0 +1,16 @@
+﻿namespace MorningAssignment;
+
+internal class AddUsingRecursion
+{
+    private static int count = 1;  
+    public  int sum = 0;
+    public void AddNumbers()
+    {
+        if (count <= 10)
+        {
+            sum += count;
+            count++;
+            AddNumbers(); 
+        }
+    }
+}
